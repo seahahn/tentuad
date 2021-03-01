deae810337936db5c8e962a0fa5a75c891353e92
seahahn <seah.ahn.nt@gmail.com>
2021-03-01 17:39:40 +0900

    main page 1st commit

---
+++ head.php
@@ -0,0 +1,9 @@
+<head>
+		<title>TentuAd: AI 광고 어시스턴트</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!-- <link rel="stylesheet" href="assets/css/main.css" /> -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css" />
+        <link rel="stylesheet" href="assets/css/landing.css" />
+		<link rel="stylesheet" href="assets/css/tentu.css" />
+	</head>
