e98953bfa2d5e0b30083c5d78ce9f26e1b72cf93
seahahn <seah.ahn.nt@gmail.com>
2021-03-01 19:13:47 +0900

    pw page set

--- pwsent.php
+++ pwsent.php
@@ -9,11 +9,8 @@
 		<title>TentuAd: AI 광고 어시스턴트</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/pwreset_chunk.css" />
+		<link rel="stylesheet" href="assets/css/pwsent_chunk.css" />
 		<link rel="stylesheet" href="assets/css/app.css" />
-        <style type="text/css"> 
-            a { text-decoration:none } 
-        </style> 
 	</head>	
     <body>
         <style type="text/css">html.hs-messages-widget-open.hs-messages-mobile,html.hs-messages-widget-open.hs-messages-mobile body{overflow:hidden!important;position:relative!important}html.hs-messages-widget-open.hs-messages-mobile body{height:100%!important;margin:0!important}#hubspot-messages-iframe-container{display:initial!important;z-index:2147483647;position:fixed!important;bottom:0!important}#hubspot-messages-iframe-container.widget-align-left{left:0!important}#hubspot-messages-iframe-container.widget-align-right{right:0!important}#hubspot-messages-iframe-container.internal{z-index:1016}#hubspot-messages-iframe-container.internal iframe{min-width:108px}#hubspot-messages-iframe-container .shadow-container{display:initial!important;z-index:-1;position:absolute;width:0;height:0;bottom:0;content:""}#hubspot-messages-iframe-container .shadow-container.internal{display:none!important}#hubspot-messages-iframe-container .shadow-container.active{width:400px;height:400px}#hubspot-messages-iframe-container iframe{display:initial!important;width:100%!important;height:100%!important;border:none!important;position:absolute!important;bottom:0!important;right:0!important;background:transparent!important}</style>
@@ -24,20 +21,12 @@
                 <div id="cover">
                     <!---->
                     <div id="content" class="centered">
-                    <div data-v-75d61d69="" class="card">
-                        <div data-v-75d61d69="" class="content">
-                            <h1 data-v-75d61d69="">TENTUPLAY</h1>
-                            <h2 data-v-75d61d69="">비밀번호 재설정</h2>
-                            <p data-v-75d61d69=""> 회원가입 시 사용하셨던 이메일 주소를 입력해 주시면, 비밀번호를 변경할 수 있는 링크가 담긴 이메일을 전송해 드립니다. </p>
-                            <form data-v-75d61d69="" style="margin-top: 3rem;">
-                                <ul data-v-75d61d69="" class="fieldset">
-                                <li data-v-75d61d69="">
-                                    <input data-v-75d61d69="" type="email" maxlength="50" class=""><label data-v-75d61d69="">이메일</label><!---->
-                                </li>
-                                </ul>
-                                <div data-v-75d61d69="" class="center aligned"><button type="button" data-v-75d61d69="" class="fluid primary button" onclick="location.href='pwsent.php'"> 이메일 전송 </button></div>
-                                <div data-v-75d61d69="" class="center aligned" style="margin-top: 2rem;"><a data-v-75d61d69="" href="login.php" class="sub"> 로그인 페이지로 돌아가기 </a></div>
-                            </form>
+                    <div data-v-0b9a799b="" class="card">
+                        <div data-v-0b9a799b="" class="content">
+                            <h1 data-v-0b9a799b="">TENTUPLAY</h1>
+                            <h2 data-v-0b9a799b="">메일함을 확인해주세요</h2>
+                            <p data-v-0b9a799b="">입력하신 이메일로 비밀번호 재설정 안내 메일이 전송되었습니다.<br>문제가 발생하였을 경우, <a href="mailto:contact@tentuplay.io">고객센터</a>로 문의해주세요.</p>
+                            <div data-v-0b9a799b="" class="center aligned" style="margin-top: 2rem;"><a data-v-0b9a799b="" href="login.php" class="fluid primary button"> 로그인 페이지로 돌아가기 </a></div>
                         </div>
                     </div>
                     </div>
