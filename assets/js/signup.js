658d08594225600c797344a095324586df787c9c
seahahn <seah.ahn.nt@gmail.com>
2021-03-04 22:37:55 +0900

    signup fixed

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -71,7 +71,7 @@ $(function(){
 /* 비밀번호 입력 및 일치 여부 검증 */
 $(function(){
     $("#pw_confirm").blur(function(){
-        var regPw = /^.*(?=^.{8,16}$)(?=.*\d)(?=.*[!@#$%^&+=]).*$/; // 비번 형식 검증식
+        var regPw = /^.*(?=^.{8,16}$)(?=.*[0-9!@#$%^&+=]).*$/; // 비번 형식 검증식
         // var regPw = /^.*(?=^.{8,16}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/; // 비번 형식 검증식
         if($("#password").val()==""){
             $("#pw_check_msg").html("비밀번호를 입력해주세요.").css("color", "red").attr("data-check", "0");
@@ -83,7 +83,6 @@ $(function(){
             $("#pw_shape").css("color", "silver").attr("data-check", "0");
             $("#pw_check_msg").html("비밀번호가 일치하지 않습니다.").css("color", "red").attr("data-check", "0");
         } else if(!regPw.test(($(this)).val()) || !regPw.test($("#password").val())) {
-            console.log(regPw.test(($(this)).val()));
             $("#pw_shape").css("color", "red").attr("data-check", "0");
             $("#pw_check_msg").html("올바른 비밀번호 형식이 아닙니다.").css("color", "red").attr("data-check", "0");
         } else {    
