d43ea993b181cd980e906bcfb0cf019f968e5ee5
seahahn <seah.ahn.nt@gmail.com>
2021-03-04 14:50:34 +0900

    folder deleted

--- tentuad-master/test/mailtest.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "tnstls93@naver.com";
-$subject = "Greeting";
-$message = "Hello\nWorld";
-$headers = 'From:noreply@tentuad.io' . "\r\n"; // Set from headers
-mail( $to, $subject, $message, $headers );
-?>
