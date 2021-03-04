0181d79c1080f6f0af32d284191d256ecf2ea912
seahahn <seah.ahn.nt@gmail.com>
2021-03-04 16:12:58 +0900

    sidebar up

--- info/add.php
+++ info/add.php
@@ -20,44 +20,11 @@ include_once "../util/db_con.php";
         <noscript>We're sorry, but TENTUPLAY doesn't work properly without JavaScript enabled. Please enable it to continue.</noscript>
         <div>
             <div id="viewport" class="blur1red">
-                <aside id="sidebar">
-                    <div class="sidebar">
-                    <nav id="gnb">
-                        <!-- 사이드바 헤더 부분 시작-->
-                        <h2 class="hidden">Menu</h2>
-                        <div class="border-bottom">
-                            <a href="dashboard.php" class="active">
-                                <h1 class="logo">TENTUPLAY</h1>
-                                <h2 class="text-center mb-3">광고주 센터</h2>
-                            </a>
-                        </div>
-                        <!-- 사이드바 헤더 부분 끝-->
-
-                        <!-- 사이드바 내용 부분 시작-->
-                        <ul class="global navigation">
-                            <li>
-                                <span data-v-4bcec590="" class="exact active">광고 목록</span>
-                                <div aria-expanded="true">
-                                <ul class="submenu level one">
-                                    <li data-v-4bcec590="">
-                                        <a data-v-4bcec590="" href="#" aria-current="page" class=""><span data-v-4bcec590="">광고 1 </span></a>
-                                    </li>
-                                    <li data-v-4bcec590="">
-                                        <a data-v-4bcec590="" href="#" class=""><span data-v-4bcec590="">광고 2</span></a>
-                                    </li>
-                                </ul>
-                                </div>
-                            </li>
-                        </ul>
-                        <!-- 사이드바 내용 부분 끝-->
-                        <button type="button" class="btn btn-lg btn-warning mt-3 justify-content-center w-100 rounded-0"><span class="text-light">+ 광고 만들기</span></button>
-                    </nav>
-                    </div>
-                </aside>
+                <?php include_once "../fragment/sidebar.php"; ?>
 
                 <div id="main">
                     <?php include_once "../fragment/profile_icon.php";?>
-                    
+
                         <div class="container">
                             <h2 class="page headline">광고 만들기</h2>
                             
--- test/test.php
+++ test/test.php
@@ -1,12 +1,42 @@
 <?php
-    session_start();
-    unset($_SESSION["email"]);
-    unset($_SESSION["username"]);
-    unset($_SESSION["role"]);
-    unset($_SESSION["idx"]);
-    echo("
-        <script>
-            // alert('로그아웃 되었습니다.');
-            location.href = 'login.php'
-        </script>")
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $con = new mysqli("127.0.0.1", "DB계정명", "DB계정PW", "DB이름");
+    $sql = "select * from user where email='$email'";
+
+    $result = mysqli_query($con, $sql);
+
+    $num_match = mysqli_num_rows($result);
+
+    if(!$num_match){
+        echo("
+            <script>
+            window.alert('등록되지 않은 이메일입니다.')
+            history.go(-1)
+            </script>
+            ");
+    } else {
+        $row = mysqli_fetch_array($result);
+        $db_userpw = $row['pw']; // DB에 저장된 사용자의 비밀번호 정보
+
+        if(!password_verify($password, $db_userpw)){
+            echo("
+                <script>
+                window.alert('비밀번호가 틀립니다.')
+                history.go(-1)
+                </script>
+                ");
+        } else {
+            session_start(); // 세션 시작. 본 예제는 DB에 사용자의 이메일, 닉네임과 함께 사용자 고유 번호를 가져왔습니다.
+            $_SESSION["useremail"] = $row["email"]; // 사용자 이메일
+            $_SESSION["usernickname"] = $row["nickname"]; // 사용자 닉네임
+            $_SESSION["num"] = $row["num"]; // 사용자 DB내의 고유 번호(PRIMARY KEY)
+            echo("
+                <script>
+                location.href = '/index.php'; // 메인 페이지로 이동
+                </script>
+                ");
+        }
+    }
 ?>
