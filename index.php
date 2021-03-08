fdc8762760247e117eda8e99362f1977dbd556b5
seahahn <seah.ahn.nt@gmail.com>
2021-03-08 12:01:59 +0900

    index.php fixed

--- index.php
+++ index.php
@@ -207,7 +207,17 @@
 			</div>
 		</footer>
 
-		<?php include_once "./util/scripts.php" ?>
+		<!-- <?php include_once "./util/scripts.php" ?> -->
+				<!-- Scripts -->
+		<script src="./assets/js/jquery.min.js"></script>
+		<script src="./assets/js/jquery.scrolly.min.js"></script>
+		<script src="./assets/js/browser.min.js"></script>
+		<script src="./assets/js/breakpoints.min.js"></script>
+		<script src="./assets/js/util.js"></script>
+		<script src="./assets/js/main.js"></script>
+		<script src="./assets/js/bootstrap.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
 
 	</body>
 </html>
